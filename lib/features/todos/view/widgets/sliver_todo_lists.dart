177eb632d6163bb20f44fd79e696639136e13415
Fischer Zsolt <fischer.zsolti@gmail.com>
2024-05-12 11:26:29 +0200

    feat: Moved text editing controller

--- lib/features/todos/view/widgets/sliver_todo_lists.dart
+++ lib/features/todos/view/widgets/sliver_todo_lists.dart
@@ -1,29 +1,16 @@
 import 'package:dubmeio/features/todos/todos.dart';
+import 'package:dubmeio/features/todos/view/widgets/generate_dummy_data.dart';
 import 'package:dubmeio/features/todos/view/widgets/todo_input_field.dart';
 import 'package:dubmeio/features/todos/view/widgets/todo_list_tile.dart';
 import 'package:dubmeio_widgets/dubmeio_widgets.dart';
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:sliver_tools/sliver_tools.dart';
 
 /// A sliver that displays a list of [Todo]s.
-class SliverTodoLists extends StatefulWidget {
+class SliverTodoLists extends StatelessWidget {
   /// Creates a [SliverTodoLists].
   const SliverTodoLists({super.key});
-
-  @override
-  State<SliverTodoLists> createState() => _SliverTodoListsState();
-}
-
-class _SliverTodoListsState extends State<SliverTodoLists> {
-  final _controller = TextEditingController();
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -31,7 +18,19 @@ class _SliverTodoListsState extends State<SliverTodoLists> {
       builder: (context, state) {
         return MultiSliver(
           children: [
-            TodoInputField(controller: _controller),
+            const TodoInputField(),
+            SliverToBoxAdapter(
+              child: Column(
+                children: [
+                  const GenerateDummyData(),
+                  Text(
+                    'Use the generation to test the SliverPinnedHeaderSection',
+                    style:
+                        CupertinoTheme.of(context).textTheme.tabLabelTextStyle,
+                  ),
+                ],
+              ),
+            ),
             SliverPinnedHeaderSection(
               title: 'Active:',
               children: state.activeTodos
--- lib/features/todos/view/widgets/todo_input_field.dart
+++ lib/features/todos/view/widgets/todo_input_field.dart
@@ -1,19 +1,28 @@
 import 'package:dubmeio/features/todos/todos.dart';
-import 'package:dubmeio/features/todos/view/widgets/generate_dummy_data.dart';
 import 'package:dubmeio_utils/dubmeio_utils.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 /// A sliver that displays a text field for adding a [Todo].
-class TodoInputField extends StatelessWidget {
+class TodoInputField extends StatefulWidget {
   /// Creates a [TodoInputField].
   const TodoInputField({
-    required TextEditingController controller,
     super.key,
-  }) : _controller = controller;
+  });
 
-  final TextEditingController _controller;
+  @override
+  State<TodoInputField> createState() => _TodoInputFieldState();
+}
+
+class _TodoInputFieldState extends State<TodoInputField> {
+  final _controller = TextEditingController();
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -22,43 +31,34 @@ class TodoInputField extends StatelessWidget {
       sliver: SliverToBoxAdapter(
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 16),
-          child: Column(
+          child: Row(
+            mainAxisSize: MainAxisSize.min,
             children: [
-              Row(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Flexible(
-                    child: CupertinoTextField(
-                      placeholder: 'Add a todo!',
-                      controller: _controller,
-                      onSubmitted: (value) {
-                        context.read<TodosBloc>().addTodo(
-                              Todo(
-                                title: value,
-                              ),
-                            );
-                        _controller.clear();
-                      },
-                      textInputAction: TextInputAction.done,
-                    ),
-                  ),
-                  if (context.isMobile)
-                    IconButton(
-                      onPressed: () {
-                        context.read<TodosBloc>().addTodo(
-                              Todo(title: _controller.text),
-                            );
-                        _controller.clear();
-                      },
-                      icon: const Icon(Icons.send_rounded),
-                    ),
-                ],
+              Flexible(
+                child: CupertinoTextField(
+                  placeholder: 'Add a todo!',
+                  controller: _controller,
+                  onSubmitted: (value) {
+                    context.read<TodosBloc>().addTodo(
+                          Todo(
+                            title: value,
+                          ),
+                        );
+                    _controller.clear();
+                  },
+                  textInputAction: TextInputAction.done,
+                ),
               ),
-              const GenerateDummyData(),
-              Text(
-                'Use the generation to test the SliverPinnedHeaderSection',
-                style: CupertinoTheme.of(context).textTheme.tabLabelTextStyle,
-              ),
+              if (context.isMobile)
+                IconButton(
+                  onPressed: () {
+                    context.read<TodosBloc>().addTodo(
+                          Todo(title: _controller.text),
+                        );
+                    _controller.clear();
+                  },
+                  icon: const Icon(Icons.send_rounded),
+                ),
             ],
           ),
         ),
